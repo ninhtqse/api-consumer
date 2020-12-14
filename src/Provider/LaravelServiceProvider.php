b6b6867594c8b71d64fe2f538f222f0a12f91e0b
Ninhtqse <ninhstrongly@gmail.com>
2020-12-14 13:50:32 +0700

    first

---
+++ src/Provider/LaravelServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Ninhtqse\ApiConsumer\Provider;
+
+use Illuminate\Support\ServiceProvider as BaseProvider;
+use Phuongtt\ApiConsumer\Router;
+
+class LaravelServiceProvider extends BaseProvider {
+
+    public function register()
+    {
+
+    }
+
+    public function boot()
+    {
+        $this->app->singleton('apiconsumer', function(){
+            $app = app();
+
+            return new Router($app, $app['request'], $app['router']);
+        });
+    }
+
+}
